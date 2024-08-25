8f704a647a7e062f26ec0fa40c15914b6bbdf2ae
kimbodle <103980156+kimbodle@users.noreply.github.com>
2024-08-25 22:20:04 +0900

    Player animation
    이동을 멈추면 front idle로 고정이 됨

---
+++ Assets/Move/MoveController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 3f;
+    Vector2 movement = new Vector2();
+    Rigidbody2D rigidbody2D;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        UpdateMoveState();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        rigidbody2D.velocity = movement * movementSpeed;
+    }
+
+    private void UpdateMoveState()
+    {
+        if (Mathf.Approximately(movement.x, 0) && Mathf.Approximately(movement.y, 0))
+        {
+            animator.SetBool("isMove", false);
+        }
+        else
+        {
+            animator.SetBool("isMove", true);
+        }
+        animator.SetFloat("xDir", movement.x);
+        animator.SetFloat("yDir", movement.y);
+    }
+}
